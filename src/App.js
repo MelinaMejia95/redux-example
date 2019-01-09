42b86063ce9efa564a68842fe60af2fd04ad1a1b
melinamejia95 <melinamejia95@gmail.com>
2019-01-09 18:30:12 -0500

    Getting posts from redux store

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
 
-import Posts from '../src/components/Posts'
-import Postform from '../src/components/Postform'
+import Posts from '../src/components/Posts';
+import Postform from '../src/components/Postform';
+import store from './store';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <Postform />
-        <hr />
-        <Posts />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+          <Postform />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
